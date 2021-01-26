d2e6969e2a841a90c29d0198983c97cddf39c399
Dimi-Dun-Morogh <darnelbro@gmail.com>
2021-01-27 01:08:26 +0200

    renderAllTasks :11

--- src/helpers/renderMsgs/renderMsgs.ts
+++ src/helpers/renderMsgs/renderMsgs.ts
@@ -8,15 +8,14 @@ const renderAllTasks = async (arrayOfTaks: [any]): Promise<string[]> => {
   // eslint-disable-next-line no-restricted-syntax
   for (const task of arrayOfTaks) {
     const { url, date, _id } = task;
-    const aucDate = new Date(date);
-    console.log(date.toLocaleString('ru-RU'));
+    const aucDate = new Date(date).toLocaleString('ru-RU', { timeZone: 'Etc/GMT-3' });
+    // console.log(date);
+    // console.log(aucDate.toLocaleTimeString('ru-RU', { timeZone: 'Etc/GMT-3' }), date);
 
     const aucInfo: IAucInfo = await parseAucInfo(url);
     const resString = `${textToEmoji('pin')}<b>АУКЦИОН</b>${textToEmoji('pin')}: ${aucInfo.lotName}\n\n${textToEmoji('lightning')}<i>заканчивается</i>${textToEmoji(
       'lightning',
-    )}: ${aucDate.toLocaleString('ru-RU', {
-      hour12: false,
-    })}\n\n${textToEmoji('saintsRow')}цена${textToEmoji('saintsRow')}: <b>${aucInfo.price}</b>\n ${url}\n id: ${_id}\n\n`;
+    )}: ${aucDate}\n\n${textToEmoji('saintsRow')}цена${textToEmoji('saintsRow')}: <b>${aucInfo.price}</b>\n ${url}\n id: ${_id}\n\n`;
     res.push(resString);
   }
   return res;
